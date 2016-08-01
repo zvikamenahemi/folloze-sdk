1badc705df7880575e06f34ef97b0d47d141bd98
Roy Peled <royp@folloze.com>
2016-08-01 14:11:05 +0300

    debug

--- src/child/ChildMessanger.ts
+++ src/child/ChildMessanger.ts
@@ -5,6 +5,7 @@ import {PortMessage} from "../common/PortMessage";
 export default class ChildMessanger extends Messanger{
 
     send(type:MessageType, data:any = null){
+        console.log("sending", type, data);
         let message = new PortMessage(type, data);
         window.parent.postMessage( message.getJSON(), "*");
     }
